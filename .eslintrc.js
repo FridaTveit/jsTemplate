e5119d08151b0b07145e29353d548250c3a6f85d
Frida Johanne Tveit <frida.tveit@softwire.com>
2018-11-29 09:49:51 +0000

    Make eslint config minimal

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,75 +9,63 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "quotes": [
-            "error",
-            "single",
-            { "allowTemplateLiterals": true }
-        ],
+        // Always end each statement with a semicolon.
+        // If you don't use semicolons then there are a few cases where your code can behave unpredictably.
+        // For more information see https://stackoverflow.com/questions/444080/do-you-recommend-using-semicolons-after-every-statement-in-javascript.
+        // https://eslint.org/docs/rules/semi
+        //
+        // To fix error: add semicolon at the end of the statement triggering the error.
         "semi": [
             "error",
             "always"
         ],
-        "camelcase": [
-            "error"
-        ],
-        "prefer-object-spread": [
-            "error"
-        ],
-        "spaced-comment": [
-            "error",
-            "always"
-        ],
+
+        // Don't do assignments in an if condition, e.g. "if (myVariable = 1) {".
+        // Doing assignments in an if condition usually indicates a mistake, i.e. the person writing it meant to use
+        // triple equals: "if (myVariable === 1) {".
+        // So using this rule can help prevent errors.
+        // https://eslint.org/docs/rules/no-cond-assign
+        //
+        // To fix error: change = to ===
         "no-cond-assign": [
             "error",
             "always"
         ],
-        "no-new-wrappers": [
-            "error"
-        ],
-        "no-eval": [
-            "error"
-        ],
-        "no-invalid-this": [
-            "error"
-        ],
-        "no-trailing-spaces": [
-            "error"
-        ],
-        "no-multiple-empty-lines": [
-            "error"
-        ],
+
+        // Don't declare variables and then never use them.
+        // https://eslint.org/docs/rules/no-unused-vars
+        //
+        // To fix error: remove the unused variable
         "no-unused-vars": [
             "error"
         ],
-        "no-redeclare": [
-            "error"
-        ],
+
+        // Don't use the 'var' keyword. The keywords 'const' or 'let' should be used instead.
+        // For more information on why you shouldnt' use 'var' see https://hackernoon.com/why-you-shouldnt-use-var-anymore-f109a58b9b70.
+        // https://eslint.org/docs/rules/no-var
+        //
+        // To fix error: change 'var' to 'let'
         "no-var": [
             "error"
         ],
-        "radix": [
-            "error",
-            "always"
-        ],
+
+        // Don't use double equals to check for equality. Instead use triple equals as that ensures that both the type
+        // and the value of the variables are equal.
+        // For more information see https://codeburst.io/javascript-double-equals-vs-triple-equals-61d4ce5a121a.
+        // https://eslint.org/docs/rules/eqeqeq
+        //
+        // To fix error: change == to ===
         "eqeqeq": [
             "error",
             "always"
         ],
-        "use-isnan": [
-            "error"
-        ],
-        "no-whitespace-before-property": [
-            "error"
-        ],
-        "one-var": [
-            "error",
-            "never"
-        ],
+
+        // If the value of a variable doesn't change, then use 'const' instead of 'let' when defining it.
+        // This will make it more obvious to people reading the code that the value of the variable won't change.
+        // https://eslint.org/docs/rules/prefer-const
+        //
+        // To fix error: change 'let' to 'const'
         "prefer-const": [
-            "error"
-        ],
-        "curly": [
             "error"
         ]
     }
